459c16386774244476e75e3efb837d73a1c98647
rlawlsdn263 <rlawlsdn263@naver.com>
2025-10-17 15:28:56 +0900

    chore: vite 빌드 옵션 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        content: resolve(__dirname, "scripts/content.ts"),
+      },
+      output: {
+        entryFileNames: (chunk) => {
+          if (chunk.name === "content") return "content.js";
+          return "assets/[name]-[hash].js";
+        },
+      },
+    },
+  },
 });
